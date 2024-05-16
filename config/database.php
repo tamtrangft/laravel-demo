a98a9fff5380e554a49a0225ed3ba0c01e4800cf
Tam, Tran <tmth@gft.com>
2024-05-16 15:39:57 +0700

    fix various

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'Thanhtam4692'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
